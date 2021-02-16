cbfed499bad4ec8e15f462e76f578e70726cf5df
git <ih8reging@comcast.net>
2021-02-16 17:22:40 -0600

    added parallax to the home page, additional images (files not committed for some reason)

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,10 +1,12 @@
 <template>
+  <div id="topBar">
   <b-navbar type="dark" class="mkpNav">
     <a href="" @click="$router.push('/')">
       <img src="../assets/MKPUSA_logo_hor.png" alt="small logo" id="leftLogo" class="navImages"/>
     </a>
     <LeftMenu/>
   </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -27,5 +29,9 @@ export default {
   max-width:25%;
   height:auto;
   max-height:100%;
+}
+
+#topBar {
+  margin-bottom: 2%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="home">
-    <div id="navDiv">
       <TopBar/>
+    <div class="parallax-1 parallax-all">
+      <p style="font-size: xx-large;">MKPs mission: text here </p>
     </div>
-    <b-container>
-      <img src="../assets/memes23b-RobertMoore-archtypal.jpg" alt="Robert Moore archetype image">
-    </b-container>
+    <div class="parallax-2 parallax-all">
+      <p class="dividerText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec orci a lorem lobortis malesuada in eget diam. Aliquam ut purus finibus, mattis quam non, rutrum urna. Phasellus feugiat massa at convallis pharetra. Duis vel lorem enim. Nullam eleifend a eros quis accumsan. Nam quis consequat elit, at iaculis dolor. Phasellus ac scelerisque urna, non tincidunt lorem. Suspendisse non ornare leo. Etiam in ultricies leo. Duis faucibus aliquet magna eu facilisis. Nulla a convallis eros, sit amet consequat nibh. Mauris nulla elit, vehicula a consequat vitae, commodo sit amet ipsum. Donec luctus a lectus at consectetur. In quis ornare arcu. Nunc mollis tristique ex, nec lacinia tortor tempus id. Aliquam finibus iaculis risus vitae posuere. </p>
+    </div>
+    <div class="parallax-3 parallax-all">
+      <p class="dividerText">MN Community Mission: text here </p>
+    </div>
+    <div class="parallax-4 parallax-all">
+      <b-form>
+        sign up form here
+        <b-checkbox aria-label="dummy checkbox">Do something?</b-checkbox>
+      </b-form>
+    </div>
   </div>
 </template>
 
@@ -19,7 +29,40 @@ name: "Home",
 </script>
 
 <style scoped>
-#navDiv{
-  margin-bottom: 2%;
+
+.parallax-all {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  /*background-size: cover;*/
+
+  min-height: 100%;
+  height: 100vh;
+}
+
+.parallax-1 {
+  background-image: url("../assets/memes23b-RobertMoore-archtypal.jpg");
+}
+
+.parallax-2 {
+  background-image: url("../assets/memes52-MKP-is-About-Presence.jpg");
+}
+
+.parallax-3 {
+  background-image: url("../assets/memes52-MKP-is-About-vision.jpg");
+}
+
+.parallax-4 {
+  background-image: url("../assets/memes37-memes-compassion.jpg");
+}
+
+.dividerText {
+  background-color: #2c3e50;
+  color: #e5f3f6;
+  font-size: x-large;
+}
+
+#home {
+  /*height: 100%;*/
 }
 </style>
